53c98fb0b17c3f6ce3e6ea51f4e1cb3294c79f62
YongamaFayo <57189890+YongamaFayo@users.noreply.github.com>
2021-04-16 12:15:04 +0200

    Update greet.js

--- dist/greet.js
+++ dist/greet.js
@@ -1,9 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function greet(firstName, lastName) {
-    
-  
-
-    return `Hello, ${firstName} ${lastName}`;
+exports.GreetInShona = exports.GreetInEnglish = exports.GreetInXhosa = exports.greet = void 0;
+function greet(person) {
+    return `Hello, ${person.firstName} ${person.lastName}`;
 }
-exports.default = greet;
+exports.greet = greet;
+class GreetInXhosa {
+    greet(name) {
+        return "Molo, " + name;
+    }
+}
+exports.GreetInXhosa = GreetInXhosa;
+class GreetInEnglish {
+    greet(name) {
+        return "Hello, " + name;
+    }
+}
+exports.GreetInEnglish = GreetInEnglish;
+class GreetInShona {
+    greet(name) {
+        return "Moro, " + name;
+    }
+}
+exports.GreetInShona = GreetInShona;
+// class greetCount implements UserGreetCounter{
+// }
